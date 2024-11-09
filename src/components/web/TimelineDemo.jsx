650cc34606f44a39905fda07a5da35057809e46f
RaniaElouni <raniaelouni421@gmail.com>
2024-11-09 11:49:07 +0100

    deplo

--- src/components/web/TimelineDemo.jsx
+++ src/components/web/TimelineDemo.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import { Timeline } from "@/components/ui/TimeLine";
-import development from "../../../public/web/development.json"
+import development from "../../../public/web/development.json";
 import Lottie from "lottie-react";
 export function TimelineDemo() {
   const data = [
@@ -96,69 +96,84 @@ export function TimelineDemo() {
             Convert designs into code, set up frontend and backend, and
             implement functionality.
           </p>
-          <div className="mb-8">
-            {[
-              "High-performance optimization",
-              "Responsive design for all devices",
-              "Lazy loading images and components",
-              
-              "SEO-friendly URL structures",
-              "Cross-browser compatibility",
-              "Client-side and server-side caching",
-            
-              "Mobile-first approach",
-              "Code splitting for faster load times",
-            ].map((item, index) => (
-              <div
-                key={index}
-                className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm"
-              >
-                ✅ {item}
-              </div>
-            ))}
+          <div className="flex flex-wrap">
+            <div className="mb-8">
+              {[
+                "High-performance optimization",
+                "Responsive design for all devices",
+                "Lazy loading images and components",
+
+                "SEO-friendly URL structures",
+                "Cross-browser compatibility",
+                "Client-side and server-side caching",
+
+                "Mobile-first approach",
+                "Code splitting for faster load times",
+              ].map((item, index) => (
+                <div
+                  key={index}
+                  className="flex gap-2 items-center text-neutral-700 dark:text-neutral-300 text-xs md:text-sm"
+                >
+                  ✅ {item}
+                </div>
+              ))}
+            </div>
+
+            <div className="grid grid-cols-2 gap-4">
+              <Lottie
+                loop
+                animationData={development}
+                play
+                className=" object-cover h-20 md:h-44 lg:h-60 w-full mb-8"
+                style={{ width: 200, height: 200 }} 
+              />
+             
+            </div>
           </div>
+        </div>
+      ),
+    },
+    {
+      title: "Deployment",
+      content: (
+        <div>
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+          Launch the website on a live server and ensure all elements function as expected.
+          </p>
 
           <div className="grid grid-cols-2 gap-4">
-            {/* <Image
-               src="/web/development.json"
+            <Image
+              src="/web/design1.webp"
               alt="hero template"
-              width={1000}
-              height={1000}
+              width={500}
+              height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            /> */}
-            <Lottie
-                    loop
-                    animationData={development}
-                    play
-            //   className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-
-                    style={{ width: 300, height: 300 }} // Adjust size as needed
-                  />
-            {/* <Image
-              src="https://assets.aceternity.com/features-section.png"
+            />
+            <Image
+              src="/web/design2.webp"
               alt="feature template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src="https://assets.aceternity.com/pro/bento-grids.png"
+              src="/web/design3.webp"
               alt="bento template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
             />
             <Image
-              src="https://assets.aceternity.com/cards.png"
+              src="/web/design4.webp"
               alt="cards template"
               width={500}
               height={500}
               className="rounded-lg object-cover h-20 md:h-44 lg:h-60 w-full shadow-[0_0_24px_rgba(34,_42,_53,_0.06),_0_1px_1px_rgba(0,_0,_0,_0.05),_0_0_0_1px_rgba(34,_42,_53,_0.04),_0_0_4px_rgba(34,_42,_53,_0.08),_0_16px_68px_rgba(47,_48,_55,_0.05),_0_1px_0_rgba(255,_255,_255,_0.1)_inset]"
-            /> */}
+            />
           </div>
         </div>
       ),
-    },
+    }
   ];
   return (
     <div className="w-full">
